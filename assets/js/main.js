d21002020e7c7d23491cf6aa2e165f991482a0a5
manoj khatri <61318643+khatrimanoj@users.noreply.github.com>
2022-06-12 18:20:36 +0530

    dashboard desktop completed

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+//responsive menu js
+// let btnToggle = document.querySelector('.menu-toggle');
+// let menuToggle = document.querySelector('.menu-collapse');
+// let menuLink = document.querySelectorAll('.menu-collapse ul li:not(.hasDropdown) a, .menu-collapse .btn');
+// btnToggle.addEventListener('click', () => {
+//     menuToggle.classList.toggle('active');
+//     btnToggle.classList.toggle('active');
+// });
+
+// menuLink.forEach(element => element.addEventListener('click', ()=> {
+//     menuToggle.classList.remove('active');
+//     btnToggle.classList.remove('active');
+// }))
+
+//# sourceMappingURL=main.js.map
